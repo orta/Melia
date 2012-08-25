2500b74d63db9e1ff22f8f7a967afc6e7a0840da
Orta Therox <orta.therox@gmail.com>
2012-08-25 13:11:04 +0100

    readme

---
+++ README.mk
@@ -0,0 +1,10 @@
+Melia, an iPad app for James Melia Photography
+----
+
+Uses a UIWebview to browse a private ZenFolio account and download all the images for offline use, also possible to buy prints etc.
+
+![Screen1](https://github.com/orta/Melia/raw/master/web/screen1.png)
+![Screen1](https://github.com/orta/Melia/raw/master/web/screen2.png)
+![Screen1](https://github.com/orta/Melia/raw/master/web/screen3.png)
+
+Licensed under BSD.
